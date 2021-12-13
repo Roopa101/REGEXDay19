8237828d4794dd22b056dd5b66f2236cb3d48573
Roopa101 <83457806+Roopa101@users.noreply.github.com>
2021-12-13 17:34:57 +0530

    [Roopa] Added: Rule1:have minimum 8 characters for Valid password

--- RegexDay19/RegexDay19/Pattern.cs
+++ RegexDay19/RegexDay19/Pattern.cs
@@ -14,7 +14,7 @@ namespace RegexDay19
         public static string regex_lastName = "^[A-Z]{1}[a-z]{2,}$";
         public static string regex_email = "^[a-z0-9]+(.[a-z0-9]+)?@[a-z]+[.][a-z]{2,3}(.[a-z]{2})?$";
         public static string regex_mobile = "^[1-9][0-9]{1,2}[ ][0-9]{10}$";
-        public static string regex_passwordRule1 = "[a-zA-Z0-9]{8,}";
+        public static string regex_passwordRule1 = "^[a-zA-Z0-9]{8,}$";
 
 
 
